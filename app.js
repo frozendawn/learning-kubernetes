2bdf0c24f02440b44d6b47dfa98078dbf6a7c848
Daniel Simeonov <frozendawn@abv.bg>
2023-10-28 23:13:35 +0300

    Docker compose update.

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const app = express();
 
 const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/task-k8s?directConnection=true&authSource=admin';
 
-// mongoose.connect(`mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongo-service:27017/task-k8s?directConnection=true&authSource=admin`, {
 mongoose.connect(MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -28,7 +27,6 @@ app.post("/create-task", async (req, res) => {
     title,
     description
   });
-  console.log("test: ", result);
 
   return res.status(201).json(result);
 })
