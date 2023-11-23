47103b01524b695ca2e76fa83bce36110969f2a8
Daniel Simeonov <frozendawn@abv.bg>
2023-11-23 17:16:56 +0200

    Adding a couple tests.

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,25 +3,46 @@ const app = require('../app');
 const Task = require("../models/task");
 const mongoose = require("mongoose");
 
-describe('Sample Test', () => {
+describe('App.js', () => {
+  afterAll(async () => {
+    await Task.deleteMany();
+    mongoose.connection.close();
+  })
 
-  describe("GET /", () => {
-    afterAll(() => mongoose.connection.close())
-    it("Should return all products", async () => {
-      const task = await Task.create({
-        title: "test task",
-        description: "test task decription"
+  it("POST /create-task - Should create a task", async () => {
+
+    const body = {
+      title: "Test title",
+      description: "Test description"
+    }
+
+    const result = await request(app)
+      .post("/create-task")
+      .send(body)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+
+    expect(result.statusCode).toBe(201);
+    expect(result._body).toEqual(
+      expect.objectContaining({
+        _id: expect.any(String),
+        title: body.title,
+        description: body.description
       })
+    )
+  })
 
-      console.log("task result: ", task);
-
-      const result = await request(app)
+  it("GET / - Should return all products", async () => {
+    const task = await Task.create({
+      title: "test task",
+      description: "test task decription"
+    })
+    const result = await request(app)
       .get("/")
       .expect('Content-Type', /json/)
 
-      expect(result._body.length).toBeGreaterThan(0);
-      expect(result.statusCode).toBe(200)
-    });
+    expect(result._body.length).toBeGreaterThan(0);
+    expect(result.statusCode).toBe(200);
   });
 
 })
